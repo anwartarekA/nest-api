1e5ed4a2ede354142a18c9ce926571801dbd0f35
= <=>
2026-07-07 03:08:51 +0100

    install typeorm and sqlie3 to work with and initialize connection to sqlite database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,18 @@ import { UserModule } from './users/user.module';
 import { ReportModule } from './reports/report.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UserModule, ReportModule],
+  imports: [
+    UserModule,
+    ReportModule,
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'db.sqlite',
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

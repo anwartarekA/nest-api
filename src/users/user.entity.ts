23b0148d545e470e084d3a1aea5698f3c4196de9
= <=>
2026-07-07 05:14:56 +0100

    connect user entity with it parent

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
-export class UserEntity {
+export class User {
   @PrimaryGeneratedColumn()
   id: number = 0;
   @Column()

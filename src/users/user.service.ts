243f618aeb9e119cfce323c998679324447b1ec1
= <=>
2026-07-07 05:26:49 +0100

    make create function

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -4,5 +4,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) repo: Repository<User>) {}
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
 }

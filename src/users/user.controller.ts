1da48fd6f1895ddcb2639fe53c4ebda233fe61b4
= <=>
2026-07-07 05:28:46 +0100

    import user service to use it as dependency for user controller

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UserService } from './user.service';
 @Controller('auth')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    return this.userService.create(body.email, body.password);
   }
 }

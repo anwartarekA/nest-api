7ba7b8efc154413b1fc071a1476a3e2431b2c9dc
= <=>
2026-07-07 03:03:07 +0100

    exporting and importing classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-
+import { UserModule } from './users/user.module';
+import { ReportModule } from './reports/report.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
-  imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [UserModule, ReportModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}

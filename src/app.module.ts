d0e9bfc71cb58de6392ffa870fb515a2b43a1dbd
= <=>
2026-07-07 02:51:57 +0100

    generate report module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ReportModule],
   controllers: [],
   providers: [],
 })

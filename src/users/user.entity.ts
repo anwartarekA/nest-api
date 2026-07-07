a245b144d350e79ce37095a03adb3676ac525d04
= <=>
2026-07-07 05:35:45 +0100

    add some hooks

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,10 +1,14 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number = 0;
+  id: number = 1;
   @Column()
   email: string = '';
   @Column()
   password: string = '';
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
 }

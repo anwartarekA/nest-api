1a643db1192bcb708af62b9a4ac4ce1d7cadeb3e
= <=>
2026-07-20 22:09:03 +0100

    serilazing properites of response

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,18 +1,17 @@
 import { ExecutionContext, CallHandler, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { UserDto } from '../users/dtos/user.dto';
+import { plainToClass } from 'class-transformer';
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('before the route handler', context);
-    return next
-      .handle()
-      .pipe(
-        map((data: any) =>
-          console.log(
-            'after the route handler, before response is sent out',
-            data,
-          ),
-        ),
-      );
+    return next.handle().pipe(
+      map((data: any) =>
+        plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        }),
+      ),
+    );
   }
 }

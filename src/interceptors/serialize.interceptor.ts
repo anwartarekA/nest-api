1c0dd31572f32b171fdfb5049a561450d168b79c
= <=>
2026-07-20 20:59:07 +0100

    compelete the structure of interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -2,5 +2,17 @@ import { ExecutionContext, CallHandler, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 export class SerializeInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('before the route handler', context);
+    return next
+      .handle()
+      .pipe(
+        map((data: any) =>
+          console.log(
+            'after the route handler, before response is sent out',
+            data,
+          ),
+        ),
+      );
+  }
 }
